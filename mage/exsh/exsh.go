34341944d0732710dbbe1a9a928ec53e1d42b6e9
Tom <tom@tdpain.net>
2021-03-26 00:34:53 +0000

    Add exsh.Command
    Signed-off-by: Tom <tom@tdpain.net>

--- mage/exsh/exsh.go
+++ mage/exsh/exsh.go
@@ -1,8 +1,17 @@
 package exsh
 
-import "os/exec"
+import (
+	"os"
+	"os/exec"
+)
 
 func IsCmdAvail(cmd string) bool {
 	_, err := exec.LookPath(cmd)
 	return err == nil
+}
+
+func Command(cs string, args ...string) *exec.Cmd {
+	cmd := exec.Command(cs, args...)
+	cmd.Stdout = os.Stdout
+	return cmd
 }
