ea44115ac7b241f069f8194f06171138a1bf688f
Tom <tom@tdpain.net>
2021-03-27 16:47:33 +0000

    Remove exsh.Command

--- mage/exsh/exsh.go
+++ mage/exsh/exsh.go
@@ -9,9 +9,3 @@ func IsCmdAvail(cmd string) bool {
 	_, err := exec.LookPath(cmd)
 	return err == nil
 }
-
-func Command(cs string, args ...string) *exec.Cmd {
-	cmd := exec.Command(cs, args...)
-	cmd.Stdout = os.Stdout
-	return cmd
-}
